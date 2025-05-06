555cf3f7d970ab8d515eb04b1957750e61da3641
unknown <bilal.asif@centricdxb.com>
2025-05-06 21:24:09 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['Front End Developer' , 'Web Designer', 'Mobile App Developer', 'Software Engineer'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true,
+    smartBackspace: true,
+    backDelay: 1500,
+    startDelay: 500
+  });
